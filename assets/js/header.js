5e88149dc77ab06609ac17527f6fc0f57c3c7d37
sabina-salehova <sabinaas@code.edu.az>
2022-07-29 00:47:58 +0400

    header-top and header-average added

---
+++ assets/js/header.js
@@ -0,0 +1,20 @@
+function showIcon(anything){
+    document.querySelector('.textbox').value=anything;
+    let arr=document.getElementsByClassName('forOption');
+
+    for (const item of arr) {
+        if(item.innerHTML==anything)
+        {
+            item.classList.add('active-color');
+        }
+        else
+        {
+            item.classList.remove('active-color');
+        }
+    }
+}
+
+let dropdownI=document.querySelector('.dropdownIcon');
+dropdownI.onclick=function(){
+    dropdownI.classList.toggle('active');
+}
