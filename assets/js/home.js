4fe41aee23adfb54d76cf18f026b0f1aac8cd81f
sabina-salehova <sabinaas@code.edu.az>
2022-07-31 20:11:37 +0400

    added smartWatch section on home page

--- assets/js/home.js
+++ assets/js/home.js
@@ -70,7 +70,7 @@ $(document).ready(function(){
            slidesToShow: 3,
            slidesToScroll: 3,
            infinite: true,
-           dots: true
+           dots: false
          }
        },
        {
@@ -106,7 +106,7 @@ $(document).ready(function(){
              slidesToShow: 3,
              slidesToScroll: 3,
              infinite: true,
-             dots: true
+             dots: false
            }
          },
          {
