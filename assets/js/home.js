8ccc9448a88299b76547edc249916c43ffccf09c
sabina-salehova <sabinaas@code.edu.az>
2022-07-31 11:22:27 +0400

    added slick-slider first time and its include div

--- assets/js/home.js
+++ assets/js/home.js
@@ -52,3 +52,44 @@ setInterval(()=>{
 
 /* ----------------------- Slick Slider -------------------------- */
 
+$(document).ready(function(){
+ $('.products-all-slickdd').slick({
+     dots: false,
+     infinite: false,
+     speed: 300,
+     slidesToShow: 3,
+     slidesToScroll: 1,
+     responsive: [
+       {
+         breakpoint: 1024,
+         settings: {
+           slidesToShow: 3,
+           slidesToScroll: 3,
+           infinite: true,
+           dots: true
+         }
+       },
+       {
+         breakpoint: 600,
+         settings: {
+           slidesToShow: 2,
+           slidesToScroll: 2
+         }
+       },
+       {
+         breakpoint: 480,
+         settings: {
+           slidesToShow: 1,
+           slidesToScroll: 1
+         }
+       }
+     ]
+   });
+
+    //   $('.products-all-slick').slick({
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     autoplay: true,
+    //     autoplaySpeed: 2000,
+    //   });
+});
